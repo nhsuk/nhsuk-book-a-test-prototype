d05d2a21bf50848a2ddaad1afb9af8c310ab9ab2
AdamChrimes <iChrimes@live.co.uk>
2020-04-21 20:59:23 +0100

    :bug: first iteration of a user journey

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'NHS.UK prototype kit',
+  serviceName: 'Get a coronavirus test',
 
   // Port to run nodemon on locally
   port: 2000,
--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,19 @@
 const express = require('express');
 const router = express.Router();
 
-// Add your routes here - above the module.exports line
+// Test kit type routing
+router.post('/action/test-kit-type', function (req, res) {
+  var testKit = req.session.data['test-kit']
+
+  if (testKit == "By visiting a drive-through test centre"){
+    res.redirect('/test-centre')
+  }
+  else if (testKit == "By requesting a home test kit"){
+    res.redirect('https://www.gov.uk')
+  } else {
+    console.log("Validation error")
+  }
+
+})
 
 module.exports = router;
