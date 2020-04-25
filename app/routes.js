bd0ce845f9be6bc4d3f0f7f71a0f0f8594b35cb4
AdamChrimes <iChrimes@live.co.uk>
2020-04-25 15:11:43 +0100

    split out the england regions question

--- app/routes.js
+++ app/routes.js
@@ -6,13 +6,13 @@ const router = express.Router();
 router.post('/action/test-kit-type', function (req, res) {
   var testKit = req.session.data['test-kit']
 
-  if (testKit == "By visiting a drive-through test centre"){
-    res.redirect('/test-centre')
+  if (testKit == "By visiting a regional test site in England"){
+    res.redirect('/test-kit-type-regions')
   }
   else if (testKit == "By requesting a home test kit"){
     res.redirect('https://www.gov.uk')
   } else {
-    console.log("Validation error")
+    res.redirect('/test-kit-type?error')
   }
 
 })
