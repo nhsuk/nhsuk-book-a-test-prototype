82c9eec5157d336fdffd0e25b079c5f4308efef6
AdamChrimes <iChrimes@live.co.uk>
2020-04-26 11:37:31 +0100

    split out the regional pages into v2

--- app/routes.js
+++ app/routes.js
@@ -2,17 +2,29 @@
 const express = require('express');
 const router = express.Router();
 
-// Test kit type routing
+// Version 1 - Test kit type routing (Country)
 router.post('/action/test-kit-type', function (req, res) {
   var testKit = req.session.data['test-kit']
 
-  if (testKit == "By visiting a regional test site in England"){
-    res.redirect('/test-kit-type-regions')
-  }
-  else if (testKit == "By requesting a home test kit"){
+  if (testKit == "By requesting a home test kit"){
     res.redirect('https://www.gov.uk')
   } else {
     res.redirect('/test-centre')
+  }
+
+})
+
+// Version 2 - Test kit type routing (Regional)
+
+router.post('/v2/action/test-kit-type', function (req, res) {
+  var testKit = req.session.data['test-kit']
+  
+  if (testKit == "By visiting a regional test site in England"){
+    res.redirect('/v2/test-kit-type-regions')
+  } else if (testKit == "By requesting a home test kit"){
+    res.redirect('https://www.gov.uk')
+  } else {
+    res.redirect('/v2/test-centre')
   }
 
 })
