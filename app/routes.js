d57ad11fdce9725cccd654748310535932c48b63
Michael Cheung <michaelcheung@nhs.net>
2020-05-26 14:49:59 +0100

    Added a v4 with updated content

--- app/routes.js
+++ app/routes.js
@@ -18,7 +18,7 @@ router.post('/v1/action/test-kit-type', function (req, res) {
 
 router.post('/v2/action/test-kit-type', function (req, res) {
   var testKit = req.session.data['test-kit']
-  
+
   if (testKit == "England"){
     res.redirect('/v2/test-kit-type-regions')
   } else if (testKit == "Request a home test kit"){
@@ -33,13 +33,26 @@ router.post('/v2/action/test-kit-type', function (req, res) {
 
 router.post('/v3/action/test-kit-type', function (req, res) {
   var testKit = req.session.data['test-kit']
-  
+
   if (testKit == "England"){
     res.redirect('/v3/test-kit-type-regions')
   } else if (testKit == "Request a home test kit"){
     res.redirect('https://key-worker-coronavirus-home-testing.service.gov.uk/')
   } else {
     res.redirect('/v3/test-centre')
+  }
+
+})
+
+
+// Version 4 - Test kit type routing (Country)
+router.post('/v4/action/test-kit-type', function (req, res) {
+  var testKit = req.session.data['test-kit']
+
+  if (testKit == "Request a home test kit"){
+    res.redirect('https://covid-a11y.herokuapp.com/home-test-ordering/start')
+  } else {
+    res.redirect('/v4/test-centre')
   }
 
 })
